78b8cacd4bd7b31188bdc40dfe37f790834dff6c
selankon <selankon@selankon.xyz>
2025-01-23 13:10:28 +0100

    Fix searchbar overlay

--- src/components/Search/SearchBar.tsx
+++ src/components/Search/SearchBar.tsx
@@ -16,7 +16,7 @@ export const SearchBar = () => {
       top={4}
       left='50%'
       transform='translateX(-50%)'
-      zIndex={1000}
+      zIndex={900}
       width='90%'
       maxWidth='600px'
       bg='white'
