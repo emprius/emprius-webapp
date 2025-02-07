252c1644142ee93b9b1eb2315f38cd914035a729
selankon <selankon@selankon.xyz>
2025-02-07 13:41:18 +0100

    Improve map tooltip

--- src/components/Search/ToolTooltip.tsx
+++ src/components/Search/ToolTooltip.tsx
@@ -1,10 +1,10 @@
-import { Button, Stack, Text } from '@chakra-ui/react'
+import { Box, Stack } from '@chakra-ui/react'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { ToolImage } from '~components/Tools/shared/ToolImage'
 import type { Tool } from '~components/Tools/types'
 import { ToolBadges } from '~components/Tools/shared/ToolBadges'
-import { Link } from 'react-router-dom'
+import { Link as RouterLink } from 'react-router-dom'
 import { ROUTES } from '~src/router/routes'
 
 interface ToolTooltipProps {
@@ -15,23 +15,27 @@ export const ToolTooltip = ({ tool }: ToolTooltipProps) => {
   const { t } = useTranslation()
 
   return (
-    <Stack spacing={2} width='200px' py={4}>
+    <Stack
+      spacing={1}
+      width='200px'
+      py={4}
+      gap={2}
+      as={RouterLink}
+      to={ROUTES.TOOLS.DETAIL.replace(':id', tool.id.toString())}
+    >
       <ToolImage imageHash={tool.images[0]} title={tool.title} isAvailable={tool.isAvailable} height='120px' />
       <Stack mt={2} direction='row' align='center' justify='space-between'>
-        <Text fontWeight='semibold' fontSize='lg' _hover={{ color: 'primary.500', textDecoration: 'none' }}>
+        <Box fontWeight='semibold' fontSize='lg'>
           {tool.title}
-        </Text>
-        <Text color='gray.600' fontSize='md' fontWeight='bold'>
+        </Box>
+        <Box color='gray.600' fontSize='md' fontWeight='bold'>
           {t('tools.cost_unit', { cost: tool.cost })}
-        </Text>
+        </Box>
       </Stack>
       <ToolBadges tool={tool} />
-      <Text fontSize='sm' color='gray.600' noOfLines={2}>
+      <Box fontSize='sm' color='gray.600' noOfLines={2}>
         {tool.description}
-      </Text>
-      <Button size='sm' as={Link} to={ROUTES.TOOLS.DETAIL.replace(':id', tool.id.toString())}>
-        {t('common.view_details')}
-      </Button>
+      </Box>
     </Stack>
   )
 }
