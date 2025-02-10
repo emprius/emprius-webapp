86200ddd3105ee19f39eae2734698ef1766fd6a9
selankon <selankon@selankon.xyz>
2025-02-10 22:13:52 +0100

    Commit missing file

---
+++ src/components/Layout/LayoutComponents.tsx
@@ -0,0 +1,15 @@
+import {Container, ContainerProps, SimpleGrid, SimpleGridProps} from '@chakra-ui/react'
+
+/**
+ * Container used on some layouts to wrap the content
+ */
+export const MainContainer = (props: ContainerProps) => (
+  <Container {...props} maxW='container.xl' py={{ base: 3, md: 6 }} />
+)
+
+/**
+ * Simple grid used to show cards
+ */
+export const ResponsiveSimpleGrid = (props: SimpleGridProps) => (
+  <SimpleGrid columns={{ base: 1, sm: 2, md: 3, lg: 3, xl: 4 }} spacing={4} autoRows='auto' {...props} />
+)
