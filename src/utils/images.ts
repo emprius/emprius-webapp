16f09b426b0cc9dce260fa27b029f7b4c71dcce5
selankon <selankon@selankon.xyz>
2025-02-11 08:57:13 +0100

    Add missing file

---
+++ src/utils/images.ts
@@ -0,0 +1,12 @@
+export const ACCEPTED_IMG_TYPES = ['image/jpeg', 'image/jpg', 'image/png']
+
+export const INPUT_ACCEPTED_IMAGE_TYPES = ACCEPTED_IMG_TYPES.join(', ')
+
+export const filterBySupportedTypes = (files: FileList) =>
+  Array.from(files).filter((file) => {
+    const isValid = ACCEPTED_IMG_TYPES.includes(file.type)
+    if (!isValid) {
+      console.warn(`File "${file.name}" was rejected. Only JPG and PNG formats are allowed.`)
+    }
+    return isValid
+  })
