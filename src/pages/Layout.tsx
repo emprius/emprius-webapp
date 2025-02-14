70eb72381d7e7dacb825fe617d593b16df774c58
selankon <selankon@selankon.xyz>
2025-02-14 12:38:12 +0100

    Show bottom menu only if authenticated

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,6 +1,7 @@
 import { Box, Hide, useColorModeValue } from '@chakra-ui/react'
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { useAuth } from '~components/Auth/AuthContext'
 import { Footer } from '~components/Layout/Footer'
 import { OfflineBanner } from '~components/Layout/OfflineBanner'
 import { PWABanner } from '~components/Layout/PWABanner'
@@ -11,6 +12,7 @@ import { ErrorBoundary } from '~src/pages/ErrorBoundary'
 
 export const Layout = () => {
   const bgColor = useColorModeValue('gray.50', 'gray.900')
+  const { isAuthenticated } = useAuth()
 
   return (
     <Box minH='100vh' px={0} display='flex' flexDirection='column' bg={bgColor}>
@@ -21,9 +23,11 @@ export const Layout = () => {
       <Box flex={1}>
         <ErrorBoundary>
           <Outlet />
-          <Hide above='md'>
-            <BottomNav />
-          </Hide>
+          {isAuthenticated && (
+            <Hide above='md'>
+              <BottomNav />
+            </Hide>
+          )}
         </ErrorBoundary>
       </Box>
       <Footer />
