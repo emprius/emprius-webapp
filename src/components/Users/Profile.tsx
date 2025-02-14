d0a393b6a88fca88d0b1b180eed3f32c58d4d451
selankon <selankon@selankon.xyz>
2025-02-14 16:02:28 +0100

    Move user ratings component

--- src/components/Users/Profile.tsx
+++ src/components/Users/Profile.tsx
@@ -1,19 +1,5 @@
 import { EditIcon } from '@chakra-ui/icons'
-import {
-  Badge,
-  Box,
-  Button,
-  Heading,
-  HStack,
-  Stack,
-  Tab,
-  TabList,
-  TabPanel,
-  TabPanels,
-  Tabs,
-  Text,
-  useColorModeValue,
-} from '@chakra-ui/react'
+import { Badge, Box, Button, Heading, HStack, Stack, Text, useColorModeValue } from '@chakra-ui/react'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { BiWallet } from 'react-icons/bi'
@@ -21,19 +7,12 @@ import { FiMail } from 'react-icons/fi'
 import { useNavigate } from 'react-router-dom'
 import { useAuth } from '~components/Auth/AuthContext'
 import { Avatar } from '~components/Images/Avatar'
-import { ElementNotFound } from '~components/Layout/ElementNotFound'
-import ErrorComponent from '~components/Layout/ErrorComponent'
-import { ResponsiveSimpleGrid } from '~components/Layout/LayoutComponents'
-import { LoadingSpinner } from '~components/Layout/LoadingSpinner'
 import { MapMarker } from '~components/Layout/MapMarker'
-import { RatingCard } from '~components/Ratings/Card'
-import { useGetReceivedRatings, useGetSubmittedRatings } from '~components/Ratings/queries'
 import { ShowRatingStars } from '~components/Ratings/ShowRatingStars'
-import { Rating } from '~components/Ratings/types'
+import { UserRatings } from '~components/Ratings/UserRatings'
 import { UserProfile as UserProfileType } from '~components/Users/types'
 import { ROUTES } from '~src/router/routes'
 import { lightText } from '~theme/common'
-import { icons } from '~theme/icons'
 
 export const UserProfile = (user: UserProfileType) => {
   const { t } = useTranslation()
@@ -92,90 +71,5 @@ export const UserProfile = (user: UserProfileType) => {
 
       {isCurrentUser && <UserRatings />}
     </Box>
-  )
-}
-
-const RatingSection = ({
-  data,
-  isLoading,
-  error,
-  emptyTitle,
-  isSubmited = false,
-  emptyDesc,
-}: {
-  data: Rating[] | undefined
-  isLoading: boolean
-  error: Error | string | null
-  emptyTitle: string
-  emptyDesc: string
-  isSubmited?: boolean
-}) => {
-  const { t } = useTranslation()
-
-  if (isLoading) {
-    return <LoadingSpinner />
-  }
-
-  if (error) {
-    return <ErrorComponent error={error} />
-  }
-
-  if (!data?.length) {
-    return <ElementNotFound icon={icons.ratings} title={t(emptyTitle)} desc={t(emptyDesc)} />
-  }
-
-  return (
-    <ResponsiveSimpleGrid>
-      {data.map((rating: Rating, index) => (
-        <RatingCard key={index} rating={rating} userId={isSubmited ? rating.fromUserId : rating.toUserId} />
-      ))}
-    </ResponsiveSimpleGrid>
-  )
-}
-
-const UserRatings = () => {
-  const { t } = useTranslation()
-  const [tabIndex, setTabIndex] = React.useState(0)
-
-  return (
-    <Box mt={6}>
-      <Heading size='md' mb={4}>
-        {t('rating.ratings')}
-      </Heading>
-      <Tabs isLazy index={tabIndex} onChange={setTabIndex}>
-        <TabList>
-          <Tab>{t('rating.submitted')}</Tab>
-          <Tab>{t('rating.received')}</Tab>
-        </TabList>
-
-        <TabPanels>
-          <TabPanel px={1}>
-            <SubmittedRatings />
-          </TabPanel>
-          <TabPanel px={1}>
-            <ReceivedRatings />
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
-    </Box>
-  )
-}
-
-const SubmittedRatings = () => {
-  const query = useGetSubmittedRatings()
-  return (
-    <RatingSection
-      {...query}
-      emptyTitle='rating.no_submitted_ratings'
-      emptyDesc='rating.no_submitted_ratings_desc'
-      isSubmited
-    />
-  )
-}
-
-const ReceivedRatings = () => {
-  const query = useGetReceivedRatings()
-  return (
-    <RatingSection {...query} emptyTitle='rating.no_received_ratings' emptyDesc='rating.no_received_ratings_desc' />
   )
 }
