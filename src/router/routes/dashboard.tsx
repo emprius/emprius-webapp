2281d0c4f321ff7c8e5aeea2a2daffc7d83f980e
selankon <selankon@selankon.xyz>
2025-02-25 10:16:21 +0100

    Improve routes file

--- src/router/routes/dashboard.tsx
+++ src/router/routes/dashboard.tsx
@@ -16,29 +16,99 @@ const BookingsPage = lazy(() => import('~src/pages/bookings/view').then((m) => (
 const UserRatingsPage = lazy(() => import('~src/pages/ratings/view').then((m) => ({ default: m.View })))
 const UsersListPage = lazy(() => import('~src/pages/users/list').then((m) => ({ default: m.List })))
 const ToolsListPage = lazy(() => import('~src/pages/tools/list').then((m) => ({ default: m.List })))
-const ToolDetailPage = lazy(() => import('~src/pages/tools/detail').then((m) => ({ default: m.Detail })))
-const UserDetail = lazy(() => import('~src/pages/users/detail').then((m) => ({ default: m.Detail })))
+
+const RatingElements = [
+  {
+    element: <TitlePageLayout />,
+    children: [
+      {
+        path: ROUTES.RATINGS.PENDING,
+        element: (
+          <SuspenseLoader>
+            <UserRatingsPage />
+          </SuspenseLoader>
+        ),
+      },
+      {
+        path: ROUTES.RATINGS.SUBMITTED,
+        element: (
+          <SuspenseLoader>
+            <UserRatingsPage />
+          </SuspenseLoader>
+        ),
+      },
+      {
+        path: ROUTES.RATINGS.RECEIVED,
+        element: (
+          <SuspenseLoader>
+            <UserRatingsPage />
+          </SuspenseLoader>
+        ),
+      },
+    ],
+  },
+]
+
+const BookingElements = [
+  {
+    element: <TitlePageLayout />,
+    children: [
+      {
+        path: ROUTES.BOOKINGS.PETITIONS,
+        element: (
+          <SuspenseLoader>
+            <BookingsPage />
+          </SuspenseLoader>
+        ),
+      },
+      {
+        path: ROUTES.BOOKINGS.REQUESTS,
+        element: (
+          <SuspenseLoader>
+            <BookingsPage />
+          </SuspenseLoader>
+        ),
+      },
+    ],
+  },
+]
+
+const DashboardForms = [
+  {
+    element: <FormLayout />,
+    children: [
+      {
+        path: ROUTES.PROFILE.EDIT,
+        element: (
+          <SuspenseLoader>
+            <EditProfile />
+          </SuspenseLoader>
+        ),
+      },
+      {
+        path: ROUTES.TOOLS.NEW,
+        element: (
+          <SuspenseLoader>
+            <ToolAddPage />
+          </SuspenseLoader>
+        ),
+      },
+      {
+        path: ROUTES.TOOLS.EDIT,
+        element: (
+          <SuspenseLoader>
+            <ToolEditPage />
+          </SuspenseLoader>
+        ),
+      },
+    ],
+  },
+]
 
 const DashboardRoutesElements = [
   {
     element: <ProtectedRoute />,
     children: [
-      {
-        path: ROUTES.TOOLS.DETAIL,
-        element: (
-          <SuspenseLoader>
-            <ToolDetailPage />
-          </SuspenseLoader>
-        ),
-      },
-      {
-        path: ROUTES.USERS.DETAIL,
-        element: (
-          <SuspenseLoader>
-            <UserDetail />
-          </SuspenseLoader>
-        ),
-      },
       {
         element: <DashboardLayout />,
         children: [
@@ -59,77 +129,19 @@ const DashboardRoutesElements = [
             ),
           },
           {
-            element: <FormLayout />,
-            children: [
-              {
-                path: ROUTES.PROFILE.EDIT,
-                element: (
-                  <SuspenseLoader>
-                    <EditProfile />
-                  </SuspenseLoader>
-                ),
-              },
-              {
-                path: ROUTES.TOOLS.NEW,
-                element: (
-                  <SuspenseLoader>
-                    <ToolAddPage />
-                  </SuspenseLoader>
-                ),
-              },
-              {
-                path: ROUTES.TOOLS.EDIT,
-                element: (
-                  <SuspenseLoader>
-                    <ToolEditPage />
-                  </SuspenseLoader>
-                ),
-              },
-            ],
+            path: ROUTES.TOOLS.LIST,
+            element: (
+              <SuspenseLoader>
+                <ToolsListPage />
+              </SuspenseLoader>
+            ),
           },
+          ...DashboardForms,
+          ...BookingElements,
+          ...RatingElements,
           {
             element: <TitlePageLayout />,
             children: [
-              {
-                path: ROUTES.BOOKINGS.PETITIONS,
-                element: (
-                  <SuspenseLoader>
-                    <BookingsPage />
-                  </SuspenseLoader>
-                ),
-              },
-              {
-                path: ROUTES.BOOKINGS.REQUESTS,
-                element: (
-                  <SuspenseLoader>
-                    <BookingsPage />
-                  </SuspenseLoader>
-                ),
-              },
-              {
-                path: ROUTES.RATINGS.PENDING,
-                element: (
-                  <SuspenseLoader>
-                    <UserRatingsPage />
-                  </SuspenseLoader>
-                ),
-              },
-              {
-                path: ROUTES.RATINGS.SUBMITTED,
-                element: (
-                  <SuspenseLoader>
-                    <UserRatingsPage />
-                  </SuspenseLoader>
-                ),
-              },
-              {
-                path: ROUTES.RATINGS.RECEIVED,
-                element: (
-                  <SuspenseLoader>
-                    <UserRatingsPage />
-                  </SuspenseLoader>
-                ),
-              },
               {
                 path: ROUTES.USERS.LIST,
                 element: (
@@ -139,14 +151,6 @@ const DashboardRoutesElements = [
                 ),
               },
             ],
-          },
-          {
-            path: ROUTES.TOOLS.LIST,
-            element: (
-              <SuspenseLoader>
-                <ToolsListPage />
-              </SuspenseLoader>
-            ),
           },
         ],
       },
--- src/router/routes/root.tsx
+++ src/router/routes/root.tsx
@@ -2,9 +2,12 @@ import { lazy } from 'react'
 import { Layout } from '~src/pages/Layout'
 import { ROUTES } from '~src/router/routes/index'
 import { SuspenseLoader } from '~src/router/SuspenseLoader'
+import { ProtectedRoute } from '~src/router/ProtectedRoute'
 
 const HomePage = lazy(() => import('~src/pages/home/view').then((m) => ({ default: m.HomePage })))
 const NotFoundPage = lazy(() => import('~src/pages/NotFoundPage').then((m) => ({ default: m.NotFoundPage })))
+const ToolDetailPage = lazy(() => import('~src/pages/tools/detail').then((m) => ({ default: m.Detail })))
+const UserDetail = lazy(() => import('~src/pages/users/detail').then((m) => ({ default: m.Detail })))
 
 const RootRoutesElements = [
   {
@@ -22,6 +25,27 @@ const RootRoutesElements = [
         <NotFoundPage />
       </SuspenseLoader>
     ),
+  },
+  {
+    element: <ProtectedRoute />,
+    children: [
+      {
+        path: ROUTES.TOOLS.DETAIL,
+        element: (
+          <SuspenseLoader>
+            <ToolDetailPage />
+          </SuspenseLoader>
+        ),
+      },
+      {
+        path: ROUTES.USERS.DETAIL,
+        element: (
+          <SuspenseLoader>
+            <UserDetail />
+          </SuspenseLoader>
+        ),
+      },
+    ],
   },
 ]
 
