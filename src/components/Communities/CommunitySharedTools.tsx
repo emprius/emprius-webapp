617244116fa5b4faafd66a053ce0f6865e18fb75
selankon <selankon@selankon.xyz>
2025-04-30 17:41:09 +0200

    Fix null aware

--- src/components/Communities/CommunitySharedTools.tsx
+++ src/components/Communities/CommunitySharedTools.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useTranslation } from 'react-i18next'
 import { useCommunityTools } from '~components/Communities/queries'
 import { useParams } from 'react-router-dom'
 import { ToolList } from '~components/Tools/List'
@@ -8,5 +7,5 @@ export const CommunitySharedTools: React.FC = () => {
   const { id } = useParams<{ id: string }>()
   const { data, isLoading, isError, error } = useCommunityTools(id!)
 
-  return <ToolList isLoading={isLoading} isError={isError} error={error} tools={data.tools} />
+  return <ToolList isLoading={isLoading} isError={isError} error={error} tools={data?.tools} />
 }
