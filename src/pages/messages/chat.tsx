25a09553d6834e716e4059cdf1e78187b761fc45
selankon <selankon@selankon.xyz>
2025-09-25 12:21:11 +0200

    Set custom titles

--- src/pages/messages/chat.tsx
+++ src/pages/messages/chat.tsx
@@ -2,10 +2,15 @@ import React from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import { ConversationView } from '~components/Messages/ConversationView'
 import { ROUTES } from '~src/router/routes'
+import { useCustomPageTitle } from '~components/Layout/Contexts/TitleContext'
+import { useUserProfile } from '~components/Users/queries'
 
 export const View = () => {
   const { userId } = useParams<{ userId: string }>()
   const navigate = useNavigate()
+
+  const { data } = useUserProfile(userId)
+  useCustomPageTitle(data?.name)
 
   if (!userId) {
     // Redirect to conversations list if no userId provided
